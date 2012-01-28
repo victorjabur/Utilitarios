9658e67eba2ccfa87193e342bc2464cff7e31bbe
Your Name <victorjabur@gmail.com>
2012-01-28 14:22:59 -0200

    ajustes

--- horas.py
+++ horas.py
@@ -15,7 +15,7 @@ def lancar(r, data, hora_inicio, hora_fim, descricao):
     task_id = '3604'
     task_name = unicode('ALOCAÇÃO', 'iso-8859-1')
     descricao = unicode(descricao, 'iso-8859-1')
-    task_log_id = '66330'
+    task_log_id = ''
 
     total_horas = (datetime.strptime(hora_fim, '%H:%M') - datetime.strptime(hora_inicio, '%H:%M')).seconds / 3600.00
     post_lancto = {'task_log_id':task_log_id, 'task_log_creator':id_criador, 'task_log_hours':total_horas, 'task_end_date':data+' '+hora_inicio+':00', 'task_log_date':data+' '+hora_inicio+':00', 'task_log_task':task_id, 'task_log_name':task_name,'start_time':hora_inicio, 'end_time':hora_fim, 'task_log_description':descricao, 'dosql':'do_updatetask'}
@@ -62,4 +62,4 @@ lista_lancto.append(lancto.lancto('2012-01-27', '11:30', '13:30', 'contestacao 1
 lista_lancto.append(lancto.lancto('2012-01-27', '14:30', '21:00', 'contestacao 11g'))
 
 for lcto in lista_lancto:
-    lancar(r, lcto.data, lcto.hora_inicio, lcto.hora_fim)
+    lancar(r, lcto.data, lcto.hora_inicio, lcto.hora_fim, lcto.descricao)
